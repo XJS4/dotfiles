dcfded56e21a7ea623fd31048256b2a5a91b5d6a
XJS4 <seb.holden@icloud.com>
2022-03-13 22:05:46 +0000

    Started using switch/case statements to determine user input

--- install.sh
+++ install.sh
@@ -8,4 +8,18 @@ else
     sudo pacman -S --needed fish zsh nvim
     cp -r ./.zshrc $HOME/
     cp -r ./config.fish $HOME/.config/fish/
+    read -p "Do you have Vim-Plug installed? (y/n) " PLUGPREF
+    case "$PLUGPREF" in
+        [yY])
+            echo "OK, I will now copy the config files."
+            cp -r ./neovim/init.vim $HOME/.config/nvim/
+            cp -r ./neovim/plugins.vim $HOME/.config/nvim/vim-plug/
+            ;;
+        [nN])
+            echo "OK, I will now install vim-plug."
+            ;;
+        *)
+            echo "Please enter y or n"
+            ;;
+    esac
 fi
