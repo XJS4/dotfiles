cdc3b7df20c5d8130622147fb4c6166cee82cb37
XJS4 <seb.holden@icloud.com>
2022-03-14 08:09:35 +0000

    Finished Neovim stuff.

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ if (( $EUID == 0 )); then
     exit
 else
     echo "Have fun using my dotfiles"
+    echo "##################################################"
+    echo "####    Just doing some preliminary checks    ####"
+    echo "##################################################"
     sudo pacman -S --needed fish zsh nvim
     cp -r ./.zshrc $HOME/
     cp -r ./config.fish $HOME/.config/fish/
@@ -14,9 +17,20 @@ else
             echo "OK, I will now copy the config files."
             cp -r ./neovim/init.vim $HOME/.config/nvim/
             cp -r ./neovim/plugins.vim $HOME/.config/nvim/vim-plug/
+            [ ! -d $HOME/.config/nvim/plug-config/ ] && mkdir $HOME/.config/nvim/plug-config/
+            cp -r ./neovim/coc.vim $HOME/.config/nvim/plug-config/
+            echo "Just remember to run :PlugInstall once you open Neovim in order to install the plugins."
             ;;
         [nN])
             echo "OK, I will now install vim-plug."
+            curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            echo "Now copying the config files."
+            cp -r ./neovim/init.vim $HOME/.config/nvim/
+            cp -r ./neovim/plugins.vim $HOME/.config/nvim/vim-plug/
+            [ ! -d $HOME/.config/nvim/plug-config/ ] && mkdir $HOME/.config/nvim/plug-config/
+            cp -r ./neovim/coc.vim $HOME/.config/nvim/plug-config/
+            echo "By the way, just remember to run :PlugInstall once you open Neovim in order to install the plugins."
             ;;
         *)
             echo "Please enter y or n"
